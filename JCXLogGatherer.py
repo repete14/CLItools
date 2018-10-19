278b0975e94324fcd6bd2fb2ce1f8ff2a836454a
Peter Olson <peter.olson@jivesoftware.com>
2018-10-18 18:35:53 -0700

    updated to ask for context, for use with eu sites

--- JCXLogGatherer.py
+++ JCXLogGatherer.py
@@ -1,7 +1,7 @@
 # script created to gather logs from Jive JCX instances and download them to your computer, in order to upload them elsewhere
 # Created by Peter Olson
-# Version: 1.3.0
-# 6/25/18
+# Version: 1.4.0
+# 10/18/18
 
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
@@ -10,18 +10,32 @@ import os
 import datetime
 import subprocess
 
+
 def collectLogs():
-    installationName = input("Installation name (i.e. jive-noc-jcx): ")
+
+    installationName = input("Installation/file name (i.e. jive-noc-jcx): ")
     namespaceName = input("Namespace (i.e. jcx-inst-4lwsszavbrnnszco5xtghv): ")
+    context = input("Context (eu or us): ")
+    if context == "eu":
+        context = "jcx-prod-eu"
+    elif context == "us":
+        context = "jcx-prod-us-east"
+    else:
+        context = "jcx-prod-us-east"
     logsNeeded = input("extra logs to collect (a = All, d = Heap dump, t = Thread dump, h = Histogram)(i.e. dt): ")
-    downloadPath = input("Path where would you like the logs put (i.e. ~/Downloads/logs/): ")
+    downloadPath = input("Destination path for files (default= ~/Downloads/logs/): ")
+    if downloadPath == '':
+        downloadPath = "~/Downloads/logs/"
+
     collecting = True
+
+
 
     while collecting:
         podName = input("Pod name (i.e. webapp-rc-mbcsm): ")
 
         # setup the base command used for all future commands
-        execPrefix = "kubectl exec -c webapp --context=jcx-prod-us-east -n " + namespaceName + " -it " + podName + ' -- bash -c "'
+        execPrefix = "kubectl exec -c webapp --context=" + context + " -n " + namespaceName + " -it " + podName + ' -- bash -c "'
         execCommand = ''
         execSuffix = '"'
 
@@ -77,7 +91,7 @@ def collectLogs():
 
         # download log gz file to local machine where specified earlier
         print("\n------------------------------   Downloading logs ------------------------------ ")
-        os.system("kubectl cp -c webapp --context=jcx-prod-us-east -n " + namespaceName + " " + podName + ":/var/log/jive/" + archiveName + " " + downloadPath + archiveName)
+        os.system("kubectl cp -c webapp --context=" + context + " -n " + namespaceName + " " + podName + ":/var/log/jive/" + archiveName + " " + downloadPath + archiveName)
 
         # delete logs and gz gathered to return to original state.
         print("\n------------------------------   Deleting logs ------------------------------ ")
