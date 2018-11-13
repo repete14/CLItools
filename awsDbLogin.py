17e757886754f0966bbf5746f797a163e8c154a1
Peter Olson <peter.olson@jivesoftware.com>
2018-11-13 12:27:22 -0800

    split off code for use as stand alone tool to log into an AWS instance's db PSQL command line.

---
+++ awsDbLogin.py
@@ -0,0 +1,65 @@
+# Script created to login to an aws site's db to perform queries
+# Created by Peter Olson
+# Version: 0.1.0
+# 10/25/18
+
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+import os
+import subprocess
+
+
+def awsDbLogin():
+    # get input data to perform operation
+    namespace_name = input("Namespace (i.e. jcx-inst-4lwsszavbrnnszco5xtghv): ")
+    context = input("Context (eu or us): ")
+
+    if namespace_name == "test":
+        namespace_name = "jcx-inst-4lwsszavbrnnszco5xtghv"
+        context = "us"
+
+    # performing tenancy purge
+
+    if context == "eu":
+        context = "jcx-prod-eu"
+    elif context == "us":
+        context = "jcx-prod-us-east"
+    else:
+        context = input("you must choose either eu or us: ")
+        exit()
+
+    # get the name of a pod
+    exec_command = "kubectl get pods --context=" + context + " -n " + namespace_name + " | awk '{print $1}'"
+    pod_name = str(subprocess.check_output(exec_command, shell=True).decode('utf-8')).split()[1]
+    print("getting db details from pod: " + pod_name)
+
+    # setup the base command used for all future commands
+    exec_prefix = "kubectl exec -c webapp --context=" + context + " -n " + namespace_name \
+                  + " -it " + pod_name + ' -- bash -c "'
+    exec_command = ''
+    exec_suffix = '"'
+
+    # get database details
+    exec_command = "cd /secrets/jive/db/app/;cat password"
+    db_pass = str(subprocess.check_output(exec_prefix + exec_command + exec_suffix, shell=True).decode('utf-8')).strip()
+
+    exec_command = "cd /secrets/jive/db/app/;cat dbname"
+    db_name = str(subprocess.check_output(exec_prefix + exec_command + exec_suffix, shell=True).decode('utf-8')).strip()
+
+    exec_command = "cd /secrets/jive/db/app/;cat username"
+    db_username = str(subprocess.check_output(exec_prefix + exec_command + exec_suffix, shell=True).decode('utf-8')).strip()
+
+    exec_command = "cd /secrets/jive/db/app/;cat port"
+    db_port = str(subprocess.check_output(exec_prefix + exec_command + exec_suffix, shell=True).decode('utf-8')).strip()
+
+    exec_command = "cd /secrets/jive/db/app/;cat host"
+    db_host = str(subprocess.check_output(exec_prefix + exec_command + exec_suffix, shell=True).decode('utf-8')).strip()
+
+    # log into sql, in order to run delete command
+    print("use the following to log in: " + db_pass)
+    exec_command = ("psql --host " + db_host + " --port=" + db_port + " --username=" + db_username + " --dbname=" + db_name)
+    os.system(exec_prefix + exec_command + exec_suffix)
+
+
+awsDbLogin()
